ad0290f61d9404c1a5878ffd3b7f6f3999009a4f
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-15 09:27:08 -0700

    red/green/blue GET Tests pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,7 +24,23 @@ describe('app routes', () => {
       .get('/red');
 
     expect(res.status).toEqual(200);
-    expect(res.text).toEqual('red');
+    expect(res.text).toEqual('<h1>red</h1>');
+  });
+
+  it('GET html with an h1 and the word green', async () => {
+    const res = await request(app)
+      .get('/green');
+
+    expect(res.status).toEqual(200);
+    expect(res.text).toEqual('<h1>green</h1>');
+  });
+
+  it('GET html with an h1 and the word blue', async () => {
+    const res = await request(app)
+      .get('/blue');
+
+    expect(res.status).toEqual(200);
+    expect(res.text).toEqual('<h1>blue</h1>');
   });
 
 
