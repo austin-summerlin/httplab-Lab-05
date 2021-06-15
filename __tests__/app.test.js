09636c50b7e970c9340a5bfd192dfd6db221929c
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-15 08:22:52 -0700

    GET route test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('getl all from / ', async () => {
+    const res = await request(app)
+      .get('/');
+
+    expect(res.text).toEqual('hi');
+  });
+
 
 });
